58e0a3a0c4270fbe064dc46f9605bf5bd50d54f1
Eduardo Aguilar <laloaps@gmail.com>
2022-09-20 17:20:09 -0400

    Connecting Products to Carts

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, presence: true
   validates :title, length: { minimum: 10,
     message: "title is not a valid size, write minimun 10 characters"  }
@@ -8,4 +10,13 @@ class Product < ApplicationRecord
     message: 'must be a URL for GIF, JPG, JPEG or PNG image.'
   }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  private
+    # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
