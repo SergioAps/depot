8406d4c4874bcb1de5fa3950cc30bac36228ad58
Eduardo Aguilar <laloaps@gmail.com>
2022-09-20 12:19:55 -0400

    add length with message validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
+  validates :title, length: { minimum: 10,
+    message: "title is not a valid size, write minimun 10 characters"  }
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|jpeg|png)\z}i,
